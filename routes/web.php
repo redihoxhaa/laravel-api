11bc3c44e55724afd917909763eb82c585f2c4a1
redihoxhaa <redi.hoxha1996@gmail.com>
2024-02-15 15:15:38 +0100

    feat: milestone 1 e 2

--- routes/web.php
+++ routes/web.php
@@ -28,10 +28,5 @@ Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(
     Route::resource('projects', ProjectController::class)->parameters(['projects' => 'project:slug']);
 });
 
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
 
 require __DIR__ . '/auth.php';
