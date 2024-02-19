bf6826fd492a38b4963cb7a6ba2a8c06d97703fc
redihoxhaa <redi.hoxha1996@gmail.com>
2024-02-19 23:53:28 +0100

    feat: rotta funzionante per searchkey

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,13 +10,46 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::with('type', 'technologies', 'status')->paginate(6);
-        return response()->json($projects);
+
+        request()->validate([
+            'key' => ['nullable', 'string', 'min:3']
+        ]);
+
+        if (request()->key) {
+            $projects = Project::where('title', 'LIKE', '%' . request()->key . '%')->paginate(6);
+        } else {
+            $projects = Project::with('type', 'technologies', 'status')->paginate(6);
+        }
+
+        if (!$projects) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Progetto non trovato'
+            ]);
+        }
+
+        return response()->json([
+            'status' => true,
+            'result' => $projects
+        ]);
     }
 
     public function show(string $slug)
     {
-        $project = Project::where('slug', $slug)->with('type', 'technologies', 'status')->first();
-        return response()->json($project);
+        $project = Project::where('slug', $slug)
+            ->with('type', 'technologies', 'status')
+            ->first();
+
+        if (!$project) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Progetto non trovato'
+            ]);
+        }
+
+        return response()->json([
+            'status' => true,
+            'result' => $project
+        ]);
     }
 }
